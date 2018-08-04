dd9f7b043d8f3a9f72cf199f74354e673cda838c
Nicolas <nzarewsky@gmail.com>
2018-08-04 18:54:19 -0300

    Add input to login

--- src/screens/MainHeader/index.js
+++ src/screens/MainHeader/index.js
@@ -1,19 +1,39 @@
 import React, { Component } from 'react';
+import { DropdownButton } from 'react-bootstrap';
 
 import logo from '../../assets/logo.png';
 
 import './styles.css';
 import HomeButton from '../HomeButton';
+import LogInTab from '../LogInTab';
+import SignUpTab from '../SignUpTap';
 
 class MainHeader extends Component {
+
   render() {
     return(
       <div className="header">
         <img src={logo} alt="cat" className="logo"></img>
         <span className="header-title">Social Cat</span>
         <div className="header-buttons">
-          <HomeButton text="Log in"/>
-          <HomeButton text="Sign up"/>
+          <DropdownButton 
+            className="header-button"
+            bsStyle="default"
+            title="Log In"
+            noCaret
+            pullRight
+          >
+            <LogInTab/>
+          </DropdownButton>
+          <DropdownButton 
+            className="header-button"
+            bsStyle="default"
+            title="Sign up"
+            noCaret
+            pullRight
+          >
+            <SignUpTab/>
+          </DropdownButton>
         </div>
       </div>
     );
