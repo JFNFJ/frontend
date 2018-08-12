cc3018c0e7c2e0e911e8c32220cca778ed86eaed
Nicolas <nzarewsky@gmail.com>
2018-08-12 18:21:53 -0300

    Short the boch

---
+++ src/screens/SocialCatDataSection/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './styles.css';
+
+class SocialCatDataSection extends Component {
+    render() {
+      return (
+        <div className="social-cat-data-section">
+        </div>
+      );
+    }
+  }
+  
+  export default SocialCatDataSection;
+  
