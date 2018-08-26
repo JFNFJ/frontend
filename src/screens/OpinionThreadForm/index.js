67cd996d03e6a1310d0c54ca3a6d9bde027d2a0f
Nicolas <nzarewsky@gmail.com>
2018-08-26 19:31:24 -0300

    A lot of changes

--- src/screens/OpinionThreadForm/index.js
+++ src/screens/OpinionThreadForm/index.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react';
 import './styles.css';
-import logo from '../../assets/logo.png';
+import threadLogo from '../../assets/ovillo.png';
 
 class OpinionThreadForm extends Component {
+  constructor(props){
+      super(props);
+      this.state = { showInput: false };
+  }
+
+  handleInputShow = function() {
+      this.setState({ showInput: !this.state.showInput })
+  }
   render() {
       return (
         <div className="opinion-thread-form">
-          <span>HOAALDALD</span>
+          <div onClick={this.handleInputShow.bind(this)} className="opinion-thread-header">
+            <img src={threadLogo} alt="Smiley face" height="42" width="42"/>
+            <div className="opinion-thread-gradient-line" />
+            <div className="opinion-thread-form-title">Nuevo Opinion Thread</div>          
+          </div>
+          { this.state.showInput && 
+            <div className="opinion-thread-form-input-section">
+              <label className="ot-name-label">
+                Topico:
+                <input className="ot-name-label-input" type="text" name="name" />
+              </label>
+              <input className="sign-button" type="submit" value="Crear" />
+            </div>}
         </div>
       );
     }
