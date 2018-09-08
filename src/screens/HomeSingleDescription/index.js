6a83ee3e8ef47848096836c2b9f6f571affd450f
Federico Bonisconti <federico.bonisconti@mercadolibre.com>
2018-09-08 09:15:10 -0300

    changed landing tags and added styles to header buttons

--- src/screens/HomeSingleDescription/index.js
+++ src/screens/HomeSingleDescription/index.js
@@ -5,7 +5,7 @@ import './styles.css';
 function HomeSingleDescription ({title,text}) {
     return(
       <div className="home-single-description">
-        <span className="home-single-title">{title}</span>
+        <h3 className="home-single-title">{title}</h3>
         <div className="home-single-text">{text}</div>
       </div>
     );
