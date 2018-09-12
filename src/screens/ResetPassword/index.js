3ade9f68488624e70eea0b39492fb5e0a3945a01
Julieta Colombo <julietanataliacolombo@yahoo.com.ar>
2018-09-12 00:25:17 -0300

    Show error and success alert

--- src/screens/ResetPassword/index.js
+++ src/screens/ResetPassword/index.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Modal, Form, FormControl, FormGroup, ControlLabel} from 'react-bootstrap';
+import {Modal, Form, FormControl, FormGroup, ControlLabel, Alert} from 'react-bootstrap';
 
 import './styles.css';
 import apiRoute from "../../config/api";
@@ -10,7 +10,10 @@ class ResetPasswordModal extends Component {
         this.handleSubmit = this.handleSubmit.bind(this);
         this.state = {
             validationState: null,
-            email: ""
+            email: "",
+            alertStyle: "",
+            alertMessage: "",
+            show: false
         };
     }
 
@@ -19,12 +22,21 @@ class ResetPasswordModal extends Component {
         let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
         return re.test(String(email).toLowerCase());
     };
+    success = function() {
+this.setState({alertStyle: "success", alertMessage: "Se ha enviado un email con la nueva contraseña"});
+    };
+    fail = function() {
+        this.setState({alertStyle: "danger", alertMessage: "Ha ocurrido un error"});
+    };
     handleResponse = response => {
-        console.log(response)
+        this.success();
+        this.handleShow();
     };
     handleSubmit = event => {
         event.preventDefault();
-        if (!this.validEmail()) {
+        if (this.validEmail()) {
+            this.setState({validationState: "success"});
+        } else {
             this.setState({validationState: "error"});
             return false;
         }
@@ -39,29 +51,49 @@ class ResetPasswordModal extends Component {
         this.setState({email: e.target.value});
     };
 
+    handleDismiss() {
+        this.setState({show: false});
+    }
+
+    handleShow() {
+        this.setState({show: true});
+    }
+
+    renderAlert = (style, message) => {
+        if (this.state.show) {
+            return (
+                <div class="onTop">
+                <Alert bsStyle={style} onDismiss={this.handleDismiss.bind(this)}>
+                {message}
+            </Alert>
+            </div>);
+        }
+    };
+
     render() {
-        return (
-            <Modal show={this.props.show} onHide={this.props.handleClose}>
-                <Modal.Header closeButton>
-                    <Modal.Title>Olvidé mi contraseña</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    <Form>
-                        <FormGroup controlId="formEmail" validationState={this.state.validationState}>
-                            <ControlLabel>Email</ControlLabel>
-                            <FormControl type="email"
-                                         placeholder="social@cat.com"
-                                         onChange={this.handleEmailChange}/>
-                        </FormGroup>
-                    </Form>
-                </Modal.Body>
-                <Modal.Footer>
-                    <button className="btn btn-social-cat pull-right" type="submit"
-                            onClick={this.handleSubmit.bind(this)}>Enviar
-                    </button>
-                </Modal.Footer>
-            </Modal>
-        );
+        return [
+                <Modal show={this.props.show} onHide={this.props.handleClose}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Olvidé mi contraseña</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        {this.renderAlert(this.state.alertStyle, this.state.alertMessage)}
+                        <Form>
+                            <FormGroup controlId="formEmail" validationState={this.state.validationState}>
+                                <ControlLabel>Email</ControlLabel>
+                                <FormControl type="email"
+                                             placeholder="social@cat.com"
+                                             onChange={this.handleEmailChange}/>
+                            </FormGroup>
+                        </Form>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <button className="btn btn-social-cat pull-right" type="submit"
+                                onClick={this.handleSubmit.bind(this)}>Enviar
+                        </button>
+                    </Modal.Footer>
+                </Modal>
+        ];
     }
 }
 
