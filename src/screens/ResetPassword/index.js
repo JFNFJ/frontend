fb9a3aa9971f2ee011391440f9d0087f41a9cb24
Julieta Colombo <julietanataliacolombo@yahoo.com.ar>
2018-09-11 23:48:44 -0300

    Validate email

--- src/screens/ResetPassword/index.js
+++ src/screens/ResetPassword/index.js
@@ -5,11 +5,29 @@ import './styles.css';
 import apiRoute from "../../config/api";
 
 class ResetPasswordModal extends Component {
+    constructor(props) {
+        super(props);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.state = {
+            validationState: null,
+            email: ""
+        };
+    }
+
+    validEmail = function () {
+        let email = this.state.email;
+        let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(String(email).toLowerCase());
+    };
     handleResponse = response => {
         console.log(response)
     };
     handleSubmit = event => {
         event.preventDefault();
+        if (!this.validEmail()) {
+            this.setState({validationState: "error"});
+            return false;
+        }
         fetch(apiRoute + 'password/reset', {
             method: 'POST',
             body: JSON.stringify({
@@ -17,7 +35,6 @@ class ResetPasswordModal extends Component {
             })
         }).then(this.handleResponse);
     };
-
     handleEmailChange = e => {
         this.setState({email: e.target.value});
     };
@@ -30,14 +47,17 @@ class ResetPasswordModal extends Component {
                 </Modal.Header>
                 <Modal.Body>
                     <Form>
-                        <FormGroup controlId="formEmail">
+                        <FormGroup controlId="formEmail" validationState={this.state.validationState}>
                             <ControlLabel>Email</ControlLabel>
-                            <FormControl type="email" placeholder="social@cat.com" onChange={this.handleEmailChange}/>
+                            <FormControl type="email"
+                                         placeholder="social@cat.com"
+                                         onChange={this.handleEmailChange}/>
                         </FormGroup>
                     </Form>
                 </Modal.Body>
                 <Modal.Footer>
-                    <button className="btn btn-social-cat pull-right" type="submit" onClick={this.handleSubmit.bind(this)}>Enviar
+                    <button className="btn btn-social-cat pull-right" type="submit"
+                            onClick={this.handleSubmit.bind(this)}>Enviar
                     </button>
                 </Modal.Footer>
             </Modal>
