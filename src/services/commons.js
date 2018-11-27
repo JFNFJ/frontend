dfacf9c2eb9f18ab6be60603fef4794af1d37a75
Joaquin "Florius" Azcarate <j@florius.com.ar>
2018-11-26 22:34:49 -0300

    mejores errores

--- src/services/commons.js
+++ src/services/commons.js
@@ -1,6 +1,8 @@
 export function handleErrors(response) {
     if (!response.ok) {
-        return response.json().then(Promise.reject, () => Promise.reject({error: "Ha ocurrido un error inesperado"}));
+        return response.json().then((e) => {
+            return Promise.reject(e);
+        }, () => Promise.reject({error: "Ha ocurrido un error inesperado"}));
     }
     return response.json();
 }
