494b1155fd94ede6ff39142a6acdf77a9d7663ab
Joaquin "Florius" Azcarate <j@florius.com.ar>
2018-11-26 21:19:32 -0300

    Mostrar un error generico si todo sale mal

--- src/services/commons.js
+++ src/services/commons.js
@@ -1,6 +1,6 @@
 export function handleErrors(response) {
     if (!response.ok) {
-        return response.json().then(Promise.reject, () => Promise.reject(response));
+        return response.json().then(Promise.reject, () => Promise.reject({error: "Ha ocurrido un error inesperado"}));
     }
     return response.json();
 }
